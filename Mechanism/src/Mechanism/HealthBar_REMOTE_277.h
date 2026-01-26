66c756a3ccd8b44ea305416c2b87d0f59f6c3753
KaiSaturn <kaisaturn2005@gmail.com>
2026-01-26 22:47:29 +0000

    working and updated ui elements

---
+++ Mechanism/src/Mechanism/HealthBar_REMOTE_277.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Core.h"
+
+// Forward declaration
+namespace Mechanism
+{
+    class SpriteRenderer;
+}
+
+namespace Mechanism
+{
+    class MECHANISM_API HealthBar
+    {
+    public:
+        struct Color
+        {
+            unsigned char r, g, b, a;
+        };
+
+        HealthBar(int maxHealth = 10, int x = 10, int y = 10, int rectWidth = 30, int rectHeight = 20, int spacing = 5);
+        ~HealthBar();
+
+        void Render(SpriteRenderer* spriteRenderer);
+
+        int GetCurrentHealth() const { return m_CurrentHealth; }
+        int GetMaxHealth() const { return m_MaxHealth; }
+        void SetCurrentHealth(int health);
+        void SetMaxHealth(int maxHealth);
+        void SetPosition(int x, int y);
+
+    private:
+        void UpdateColor();
+        void Initialize(SpriteRenderer* spriteRenderer);
+
+        int m_CurrentHealth;
+        int m_MaxHealth;
+        int m_X;
+        int m_Y;
+        int m_RectWidth;
+        int m_RectHeight;
+        int m_Spacing;
+
+        Color m_CurrentColor;
+        Color m_HighHealthColor;
+        Color m_MediumHealthColor;
+        Color m_LowHealthColor;
+
+        unsigned int m_VAO;
+        unsigned int m_VBO;
+        unsigned int m_WhiteTexture;
+        unsigned int m_ShaderProgram;
+        bool m_Initialized;
+        int m_ScreenWidth;
+        int m_ScreenHeight;
+    };
+}
